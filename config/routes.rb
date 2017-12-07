f171a00134b0de0893768c0310591ef75067e993
DeniSerb <Dondo788@gmail.com>
2017-12-07 11:18:58 +0200

    Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
 get 'users/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   root 'application#hello'
 end
