10aa4fe3d82ff4368fa4093ffae9c938be96f96f
DeniSerb <Dondo788@gmail.com>
2017-12-13 16:22:44 +0200

    Add activation(nope)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
-    else
-      flash.now[:danger] = 'Invalid email/password combination'
-      render 'new'
-    end
+      user = User.find_by_email(params[:email])
+      if user && user.authenticate(params[:password])
+      if user.email_confirmed
+          sign_in user
+        redirect_back_or user
+      else
+        flash.now[:error] = 'Please activate your account by following the 
+        instructions in the account confirmation email you received to proceed'
+        render 'new'
+      end
+      else
+        flash.now[:error] = 'Invalid email or password' # Not quite right!
+        render 'new'
+      end
   end
 
   def destroy
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171213072934) do
+ActiveRecord::Schema.define(version: 20171213132443) do
 
   create_table "goals", force: :cascade do |t|
     t.string "title"
@@ -30,6 +30,11 @@ ActiveRecord::Schema.define(version: 20171213072934) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
+    t.string "remember_digest"
+    t.boolean "email_confirmed", default: false
+    t.string "confirm_token"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
