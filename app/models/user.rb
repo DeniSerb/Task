7d337dcd326b3294f69697920559d177b49be353
DeniSerb <Dondo788@gmail.com>
2017-12-25 11:07:05 +0200

    Models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,12 @@
 class User < ApplicationRecord
   before_create :confirmation_token
   has_many      :goals
-
   before_save { self.email = email.downcase }
   validates :first_name, :last_name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format:{ with: VALID_EMAIL_REGEX },
                     uniqueness:{ case_sensitive: false }
-
    has_secure_password
    validates :password, presence: true, length: { minimum: 6 }
 
