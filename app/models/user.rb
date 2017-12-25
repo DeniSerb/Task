1b855e4c22330673805a3ec766dea47a6170bba9
DeniSerb <Dondo788@gmail.com>
2017-12-25 11:04:24 +0200

    Format

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,18 +12,18 @@ class User < ApplicationRecord
    has_secure_password
    validates :password, presence: true, length: { minimum: 6 }
 
-   def full_name
+  def full_name
   "#{first_name} #{last_name}"
   end
 
-   def email_activate
+  def email_activate
     self.email_confirmed = true
     self.confirm_token = nil
     save!(:validate => false)
   end
 
-   private
-def confirmation_token
+  private
+    def confirmation_token
       if self.confirm_token.blank?
           self.confirm_token = SecureRandom.urlsafe_base64.to_s
       end
